14b4b9e9cf36318c4622466248aa9bdb30477fce
Yejun Park <smartyjpark@gmail.com>
2017-07-19 20:43:45 +0900

    makeObj modify

--- 0718_fresh/makeObj.js
+++ 0718_fresh/makeObj.js
@@ -1,18 +1,13 @@
 function makeObj(obj, parent){
 	var newObj = Object.create(parent)	
-	console.log(newObj);
   	Object.keys(obj).forEach(function(key){
   		newObj[key] = obj[key]
-		// const val = obj[key];
-		// console.log({key : val})
-		// newObj = Object.assign({key : val}, obj)
 	});
-	console.log(newObj);
 	return newObj;
 }
 
 var healthObj = { showHealth : function() {
-console.log(this.name + "asdfasdf "+this.lastTime)}
+console.log(this.name + "asdfasdf "+this.lastTime + "okok")}
 }
 
 var myHealth = makeObj({ name : "asdfasdf", lastTime : "23:10"}, healthObj);
