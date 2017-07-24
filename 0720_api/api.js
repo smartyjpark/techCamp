7b0638a24e9ce406f8e8d97aebe626d1e0aeca59
Yejun Park <smartyjpark@gmail.com>
2017-07-24 16:12:05 +0900

    badge and template add

--- 0720_api/api.js
+++ 0720_api/api.js
@@ -1,76 +1,28 @@
 class Tab {
-	constructor(url){
-		this.url = "http://52.78.212.27:8080/woowa/best/"
-		this.eventtarget = document.querySelector('#best-tabs');
+	constructor(){
+		this.url = "http://52.78.212.27:8080/woowa/"
+		this.eventTarget = document.querySelector('#best-tabs');
 		this.tabCache = {};
 
 	}
 
 	eventOn(){
-		this.eventtarget.addEventListener("click", function(e){
+		this.eventTarget.addEventListener("click", function(e){
 			document.querySelector("#best-tabs a.now").classList.remove("now")
-			const target = e.target
-			target.classList.toggle("now")
-			this.cacheCheck(target.id)
+			const target = e.target;
+			target.classList.toggle("now");
+			this.cacheCheck(target.id);
 
 		}.bind(this));
-	}
-
-	modalOn(id){
-		const prd = document.querySelector("#best-content-ul")
-		prd.addEventListener("click", function(e){
-			const modalId = e.target.parentNode.id;
-			this.modalAjax(modalId)
-		}.bind(this))
-
-	}
-
-
-	modalAjax(id){
-		const targetId = id;
-		const requestAjax = new XMLHttpRequest();
-		requestAjax.addEventListener("load", function(e){
-			const data = JSON.parse(requestAjax.responseText);
-			this.setModal(data)
-
-		}.bind(this))
-		requestAjax.open("GET", "http://52.78.212.27:8080/woowa/detail/"+id)
-		requestAjax.send()
-	}
-
-	setModal(data){
-		const modalData = data.data;
-		console.log(modalData)
-		document.querySelector(".best-modal").innerHTML = "";
-		const source = document.querySelector("#modal-contents").innerHTML;
-		const insertTemplate = Handlebars.compile(source);
-		const html = insertTemplate(modalData);
-		document.querySelector(".best-modal").innerHTML += html;
-
-		var modal = document.getElementById('myModal')
-		var span = document.getElementsByClassName("close")[0];
-		modal.style.display = "block";
-	    console.log(modal)
-	    span.onclick = function() {
-	    	modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		    if (event.target == modal) {
-		        modal.style.display = "none";
-		    }
-		}
-
 	}
 
 	cacheCheck(id){
 		if (this.tabCache[id]){
 			this.contentOn(this.tabCache[id])
-			console.log("cache data output!")
+			console.log("cache data output!");
 		}
 		else {
-			this.tabAjax(id)
+			this.tabAjax(id);
 		}
 	}
 
@@ -80,21 +32,70 @@ class Tab {
 		requestAjax.addEventListener("load", function(e){
 			const data = JSON.parse(requestAjax.responseText);
 			this.tabCache[id]=data;
-			console.log("cache save!")
-			this.contentOn(data)
-			this.modalOn(targetId)
+			console.log("cache save!");
+			this.contentOn(data, targetId);
+			
 
 		}.bind(this))
-		requestAjax.open("GET", this.url+id)
-		requestAjax.send()
+		requestAjax.open("GET", this.url+"best/"+id);
+		requestAjax.send();
 	}
 
-	contentOn(data){
-		document.querySelector("#best-container").innerHTML = ""
-		const source = document.querySelector("#tab-contents").innerHTML
+	contentOn(data, id){
+		const source = document.querySelector("#tab-contents").innerHTML;
 		const insertTemplate = Handlebars.compile(source);
 		const html = insertTemplate(data);
-		document.querySelector("#best-container").innerHTML += html;
+		document.querySelector("#best-container").innerHTML = html;
+		this.modalOn(id);
+	}
+
+	modalOn(id){
+		const prd = document.querySelector("#best-content-ul")
+		prd.addEventListener("click", function(e){
+			const modalId = e.target.parentNode.id;
+			this.modalAjax(modalId);
+		}.bind(this));
+
+	}
+
+
+	modalAjax(id){
+		const targetId = id;
+		const requestAjax = new XMLHttpRequest();
+		requestAjax.addEventListener("load", function(e){
+			const data = JSON.parse(requestAjax.responseText);
+			this.setModal(data);
+
+		}.bind(this))
+		requestAjax.open("GET", this.url+"detail/"+id);
+		requestAjax.send();
+	}
+
+	setModal(data){
+		const modalData = data.data;
+		// console.log(modalData)
+		const source = document.querySelector("#modal-contents").innerHTML;
+		const insertTemplate = Handlebars.compile(source);
+		const html = insertTemplate(modalData);
+		document.querySelector(".best-modal").innerHTML = html;
+		this.modalEvent()
+
+	}
+
+	modalEvent(){
+		const modal = document.getElementById('myModal');
+		const span = document.getElementsByClassName("close")[0];
+
+		modal.style.display = "block";
+	    span.onclick = function() {
+	    	modal.style.display = "none";
+		}
+
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}
 	}
 
 
@@ -139,7 +140,7 @@ class Slider{
 
 	targetDotControl(){
 		const targetDot = this.dotsList[this.index];
-		console.log(this.index)
+		// console.log(this.index)
 		targetDot.style.zindex = 50;
 		targetDot.style.display="inline-block"
 		targetDot.style.opacity = 1;
@@ -149,7 +150,7 @@ class Slider{
 
 	dotsInnerEvent(e, animate){
 		this.index = e.target.innerHTML;
-		requestAnimationFrame(animate)
+		requestAnimationFrame(animate);
 	}
 
 	setSlider(){
@@ -160,11 +161,11 @@ class Slider{
 	arrowIndex(e){
 		if (e.target === this.slideNext){
 			this.index ++;
-			this.arrowOverFlow()
+			this.arrowOverFlow();
 		}
 		if (e.target === this.slidePrev){
 			this.index --;
-			this.arrowOverFlow()
+			this.arrowOverFlow();
 		}
 
 	}
@@ -180,79 +181,57 @@ class Slider{
 
 	arrowInnerEvent(e, animate){
 		this.arrowIndex(e);
-		console.log("animate")
-		requestAnimationFrame(animate)
+		// console.log("animate")
+		requestAnimationFrame(animate);
 	}
+
+	startSlider(){
+		const freshSlider = new Slider("freshSlider");
+		freshSlider.dotsEventOn(freshSlider.dotEventSelector, animateByDot);
+		freshSlider.arrowEventOn(freshSlider.arrowEventSelector, animateByArrow);
+
+		function animateByDot(){
+			freshSlider.setSlider();
+		}
+
+		function animateByArrow(){
+			freshSlider.setSlider();
+
+		}
+
+		function arrowNext(){
+			freshSlider.index ++;
+			freshSlider.arrowOverFlow()
+			requestAnimationFrame(animateByArrow);
+		}
+
+		function autoPlay(){
+			setInterval(function(){
+				// console.log("auto check")
+				if(freshSlider.autoCheck){
+					arrowNext()
+				}
+				else{
+					freshSlider.autoCheck = 1;
+				}
+			},3000)
+		}
+
+		autoPlay()
+	}
+
 
 }
 
 document.addEventListener('DOMContentLoaded', function(){
 
+	//tab
 	const tabs = new Tab();
 	tabs.tabAjax(17010200);
 	tabs.eventOn();
 
-	const freshSlider = new Slider("freshSlider");
-	freshSlider.dotsEventOn(freshSlider.dotEventSelector, animateByDot);
-	freshSlider.arrowEventOn(freshSlider.arrowEventSelector, animateByArrow);
 
-	function animateByDot(){
-		freshSlider.setSlider();
-	}
-
-	function animateByArrow(){
-		freshSlider.setSlider();
-
-	}
-
-	function arrowNext(){
-		freshSlider.index ++;
-		freshSlider.arrowOverFlow()
-		requestAnimationFrame(animateByArrow);
-	}
-
-	function autoPlay(){
-		setInterval(function(){
-			console.log("auto check")
-			if(freshSlider.autoCheck){
-				arrowNext()
-			}
-			else{
-				freshSlider.autoCheck = 1
-			}
-		},3000)
-	}
-
-	autoPlay()
-
-		// Get the modal
-	var modal = document.getElementById('myModal');
-	console.log(modal)
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on the button, open the modal 
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	    console.log(modal)
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}
-
+	Slider.prototype.startSlider();
 
 });
 
